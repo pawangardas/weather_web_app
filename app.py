eb6bf6ae2f9079d36ee9cd4a81875bc7692dc131
pawangardas <pawangardas17@gmail.com>
2026-05-02 14:55:13 +0530

    little changes

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def home():
             url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
             response = requests.get(url)
 
-             if response.get("cod") == 200:
+            if response.get("cod") == 200:
                 weather_data = {
                 "city": response["name"],
                 "temp": response["main"]["temp"],
@@ -30,7 +30,7 @@ def home():
 
             weather_list.append(weather_data)
 
-    return render_template('index.html', weather=weather_data)
+    return render_template('index.html', weather=weather_data, weather_list=weather_list)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
