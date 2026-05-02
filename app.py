f12a0bb81ad6bd21c43a99d6a368a171eba61380
pawangardas <pawangardas17@gmail.com>
2026-05-02 14:45:52 +0530

    little changes

--- app.py
+++ app.py
@@ -10,27 +10,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    weather_list = []
     weather_data = None
     if request.method == 'POST':
         city = request.form.get('city')
-        if city:
-            api_key = os.getenv("API_KEY")  # Replace with your OpenWeatherMap API key
-            print("API KEY:", api_key, flush=True)
             
             url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
             response = requests.get(url)
 
-            print(response.text)
-            if response.status_code == 200:
-                data = response.json()
-                weather_data = {
-                    'city': data['name'],
-                    'temperature': data['main']['temp'],
-                    'description': data['weather'][0]['description'],
-                    'icon': data['weather'][0]['icon']
-                }
-            else:
-                weather_data = {'error': 'City not found'}
+                    if response.get("cod") == 200:
+            weather_data = {
+                "city": response["name"],
+                "temp": response["main"]["temp"],
+                "desc": response["weather"][0]["description"]
+            }
+
+            weather_list.append(weather_data)
+
     return render_template('index.html', weather=weather_data)
 
 if __name__ == '__main__':
