61a40aab26aebce7579996b6eb32a78b4f449432
pawangardas <pawangardas17@gmail.com>
2026-05-03 02:14:58 +0530

    GUI and code changes

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import os
 
 load_dotenv()
 
+API_KEY = os.getenv("API_KEY")
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,21 +17,28 @@ def home():
     weather_data = None
 
     if request.method == 'POST':
-        city = request.form.get('city')
+
+        if request.form.get('remove'):
+           city_to_remove = request.form.get("remove")
+           weather_list = [w for w in weather_list if w["city"] != city_to_remove]
+        
+        else:
+            city = request.form.get('city')
+
+            if city:    
+               url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+               response = requests.get(url).json()
+
+               if response.get("cod") == 200:
+                  weather_data = {
+                      "city": response["name"],
+                      "temp": response["main"]["temp"],
+                      "desc": response["weather"][0]["description"]
+                }
+                  
+    if not any(w["city"] == response["name"] for w in weather_list):
+        weather_list.append(weather_data)    
          
-        if city:     
-            url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
-            response = requests.get(url)
-
-            if response.get("cod") == 200:
-                weather_data = {
-                "city": response["name"],
-                "temp": response["main"]["temp"],
-                "desc": response["weather"][0]["description"]
-            }
-
-            weather_list.append(weather_data)
-
     return render_template('index.html', weather=weather_data, weather_list=weather_list)
 
 if __name__ == '__main__':
